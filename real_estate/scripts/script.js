001c83dacfcef9da72c33673b0d5a8a23983bc47
annuu1 <mannusng876@gmail.com>
2024-12-31 20:32:14 +0530

    done visits

--- real_estate/scripts/script.js
+++ real_estate/scripts/script.js
@@ -1,1 +1,1 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwxhxbt5bw2dL-Jdx4OkoxEZ-NkfueN9QOElxtB_3URgTc4Qx-6ljOrjwXeoCYQoEd6xw/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwjfceoeNeTOOPZY-ijuz6ZeINsjPC5BvfPUYJ9YAhJBU6RXd1CR8Ep6NEIExkWcwrPAQ/exec'
