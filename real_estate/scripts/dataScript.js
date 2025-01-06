602bcaac04d0545292b3c684b26e738fcd51c84c
annuu1 <mannusng876@gmail.com>
2025-01-06 16:29:42 +0530

    Upgraded functionality that the data refreshes after the updation

--- real_estate/scripts/dataScript.js
+++ real_estate/scripts/dataScript.js
@@ -1,3 +1,16 @@
-function updateClientDetails(){
-    
+async function updateClientDetails(dataURL){
+    try {
+        const response = await fetch(dataURL);
+        const data = await response.json();
+        if (data.success) {
+            alert("Client details submitted successfully!");
+            await loadAndUpdateData()
+            window.location.reload()
+        } else {
+            alert("Failed to submit client details.");
+        }
+    } catch (error) {
+        console.error("Error submitting data:", error);
+          alert("An error occurred while submitting the form.");
+    }
 }
